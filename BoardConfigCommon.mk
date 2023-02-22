aca18b5218a25eaab70da813da58fc6296d8feb8
Chris Goldsworthy <quic_cgoldswo@quicinc.com>
2023-02-22 09:39:23 -0800

    sm8650-common: Add a config property for the SMMU proxy driver
    Add a board config property to indicate that the SMMU proxy driver is
    present on this vendor SI in the securemsm-kernel folder. The display
    SI depends on symbols from the SMMU proxy driver, such that the
    display SI is also used across multiple targets, some of which will
    not contain the SMMU proxy driver. Expose TARGET_USES_SMMU_PROXY to
    allow the display SI to control at compile time whether they should
    use symbols from the proxy driver or not.
    
    Change-Id: Ibb94849d7fa2d9078c95a03ce2561973556c6119

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,6 +142,9 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # SEPolicy
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
+# SMMU
+TARGET_USES_SMMU_PROXY := true
+
 # UFS
 SOONG_CONFIG_NAMESPACES += ufsbsg
 SOONG_CONFIG_ufsbsg += ufsframework
